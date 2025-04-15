fc776931a9cce5a8ae5a78dd9d8dcd3680173f9f
sachior <rajendrasachio@gmail.com>
2025-04-15 10:40:25 +0700

    nilai.js

--- nilai.js
+++ nilai.js
@@ -1,22 +1,25 @@
 let grade = [
-    {grade : "A", nilai : "100"}
-    {grade : "B", nilai : "85"}
-    {grade : "C", nilai : "75"}
-]
-grade.forEach(function(item) {
-    let hasilgrade = "";
+    {grade: "A", nilai: "100"},
+    {grade: "B", nilai: "85"},
+    {grade: "C", nilai: "75"}
+];
 
-    switch (item.grade) {
-        case "A"
-        hasilgrade ="sangat baik"
-        break;
-        case "B"
-        hasilgrade ="baik"
-        break;
-        case "C"
-        hasilgrade ="kurang baik"
-        break;
-        default:
-    }
-    console.log(`${item.grade} nilai ${hasilgrade}`)
-}
+grade.forEach(function(item) {
+    let hasilgrade = "";
+
+    switch (item.grade) {
+        case "A":
+            hasilgrade = "sangat baik";
+            break;
+        case "B":
+            hasilgrade = "baik";
+            break;
+        case "C":
+            hasilgrade = "cukup baik";
+            break;
+        default:
+            hasilgrade = "nilai kosong";
+    }
+
+    console.log(`${item.grade} mendapat ${hasilgrade}`);
+});
